f1b1b4fdb1d1eea86e8f43a56513d850ecbc54b4
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-03-19 17:46:49 -0600

    Firebase added to html and javascript.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,10 @@
-$(function () {
-    $('#datetimepicker5').datetimepicker({
-        use24hours: true,
-        format: 'HH:mm'
-    });
-});
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC_Sn5HKzXOJK4LhYCAC7IpZg_RNAjbh8k",
+    authDomain: "awesome-train-scheduler.firebaseapp.com",
+    databaseURL: "https://awesome-train-scheduler.firebaseio.com",
+    projectId: "awesome-train-scheduler",
+    storageBucket: "",
+    messagingSenderId: "787171998514"
+};
+firebase.initializeApp(config);
